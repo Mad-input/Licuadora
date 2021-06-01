e991482d121c26dcc5b3e760f5fe35a57c1a7669
Mad-input <79423186+Mad-input@users.noreply.github.com>
2021-06-01 12:49:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let estadoLicuadora = false;
 const btnLicuadora = document.getElementById('blenderBtn');
 const blender = document.getElementById('blender');
+// * Sounds
+const btnSound = document.getElementById('blenderBtnSound');
+const blenderSound = document.getElementById('blenderSound');
 
 function controlarLicuadora() {
+    // ? on - off  
     blender.classList.toggle('active');
     !estadoLicuadora ? estadoLicuadora = true : estadoLicuadora = false;
+    // ? PlaySound
+    if (blenderSound.paused && btnSound.paused) {
+        blenderSound.play()
+        btnSound.play()
+    } else {
+        blenderSound.pause()
+        btnSound.play()
+        blenderSound.currentTime = 0;
+    }
 }
